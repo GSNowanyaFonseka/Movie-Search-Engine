d275a4bcba7d8e448bb458265dd6339b3831e2cd
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-05-09 17:28:26 +0530

    Implement state handling for search input on homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import MovieCard from './Components/MovieCard'
+import Home from './pages/Home';
 
 function App(){
+  const movieNumber = 2;
+
   return(
     <>
-      <MovieCard movie={{ title:"Totoro the neighbour", release_date:"2024"}}/>
-      <MovieCard movie={{title:"Harry Potter", release_date:"2022"}}/>
+      <Home />
     </>
-  );
+  ); 
 }
 
 export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import MovieCard from "../Components/MovieCard";
+import {useState} from 'react';
+
+function Home(){
+
+    const [searchQuery,setSearchQuery] = useState('');
+
+    const movies = [
+        {id:1, title:"Totoro the neighbour", release_date:"2020"},
+        {id:2, title:"Encanto", release_date:"2021"},
+        {id:3, title:"Minions", release_date:"2022"},
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    }
+
+    return(
+        <div className="home">
+
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for movies....." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}></input> 
+                <button type="submit" className="submit-button"> Search </button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map(
+                    (movie) => 
+                        movie.title.toLowerCase().startsWith(searchQuery) && (
+                            <MovieCard movie={movie} key={movie.id}/>
+                        )
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
